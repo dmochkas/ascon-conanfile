08dc12a23f3bb1558e97cb0a7b980c8bf5464656
Dmytro Ochkas <dmytro.ochkas@imt-atlantique.fr>
2025-10-01 22:42:32 +0200

    Update ascon-c package to be able to use several libraries
    Supports only x86_64 for the moment
    
    Waiting for the next ascon release to not rely on the main tag

--- conanfile.py
+++ conanfile.py
@@ -6,17 +6,17 @@ import os
 
 
 class AsconConanRecipe(ConanFile):
-    name = "ascon"
-    # Adjust version as appropriate (could be synchronized with upstream tag)
-    version = "1.0.0"
+    name = "ascon-c"
+    version = "1.3.0"
     license = "CC0-1.0"
     url = "https://github.com/ascon/ascon-c"
     description = "Reference, optimized and masked C/ASM implementations of Ascon (NIST SP 800-232)"
     topics = ("cryptography", "ascon", "aead", "hash", "mac", "prf")
     settings = "os", "arch", "compiler", "build_type"
     package_type = "library"
+    user = "dochkas"
+    channel = "experimental"
 
-    # Comma separated lists to stay user friendly for CLI overrides.
     options = {
         "fPIC": [True, False],
         "shared": [True, False],
@@ -47,6 +47,24 @@ class AsconConanRecipe(ConanFile):
         "optimized": "speed"
     }
 
+    alg_to_root_dir = {
+        "asconaead128": "crypto_aead",
+        "asconhash256": "crypto_hash",
+        "asconxof128": "crypto_hash",
+        "asconcxof128": "crypto_cxof",
+        "asconaeadxof128": "crypto_aead_hash",
+        "asconmacv13": "crypto_auth",
+        "asconprfv13": "crypto_auth",
+        "asconprfsv13": "crypto_auth"
+    }
+
+
+    algs = []
+
+    @property
+    def impl(self):
+        return "opt64" + ("_lowsize" if self.options.optimized == "size" else "")
+
     def source(self):
         # Please, be aware that using the head of the branch instead of an immutable tag
         # or commit is a bad practice and not allowed by Conan
@@ -67,16 +85,15 @@ class AsconConanRecipe(ConanFile):
     def generate(self):
         tc = CMakeToolchain(self)
 
-        algs = ';'.join((k[5:] for k, v in self.options.items() if k.startswith("with_ascon") and getattr(self.options, k) == True))
-        self.output.info(f"Configuring the following algorithms: {algs}")
+        self.algs = [k[5:] for k, v in self.options.items() if k.startswith("with_ascon") and getattr(self.options, k) == True]
+        algs_param = ';'.join(self.algs)
+        self.output.info(f"Configuring the following algorithms: {algs_param}")
 
         if self.settings.arch != "x86_64":
             raise ConanInvalidConfiguration(f"Unsupported architecture {self.settings.arch}")
 
-        impl = "opt64" + ("_lowsize" if self.options.optimized == "size" else "")
-
-        tc.variables["ALG_LIST"] = algs
-        tc.variables["IMPL_LIST"] = impl
+        tc.variables["ALG_LIST"] = algs_param
+        tc.variables["IMPL_LIST"] = self.impl
         if not self.options.with_tests:
             tc.variables["TEST_LIST"] = ""
 
@@ -90,9 +107,12 @@ class AsconConanRecipe(ConanFile):
     def package(self):
         # License
         copy(self, "LICENSE", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
-        # Headers (all .h headers from source tree)
-        include_dst = os.path.join(self.package_folder, "include")
-        copy(self, "*.h", dst=include_dst, src=os.path.join(self.source_folder, "ascon-c"), keep_path=True)
+
+        # Headers
+        for alg in self.algs:
+            include_dst = os.path.join(self.package_folder, "include", alg)
+            copy(self, "*.h", dst=include_dst, src=os.path.join(self.source_folder, self.alg_to_root_dir[alg], alg, self.impl), keep_path=True)
+
         # Libraries
         lib_dst = os.path.join(self.package_folder, "lib")
         for pattern in ("*.a", "*.lib", "*.so", "*.dylib", "*.dll"):
